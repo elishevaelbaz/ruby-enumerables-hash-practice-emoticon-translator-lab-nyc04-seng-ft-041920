d48745cb6e629345951612b8b11087c4313354dd
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-27 19:46:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,13 @@ require 'yaml'
 
 def load_library(file_path)
   # code goes here
-  YAML.load_file(file_path)
+  new_hash = {}
+  emoticons = YAML.load_file(file_path)
+  
+  emoticons.each_pair do |name, symbol_array|
+      new_hash[name] ={:english => symbol_array[0], :japanese => symbol_array[1]}
+  end
+  
   
 end
 
