4b3789813b1d3b2fb2a762f49afb5645a7b729cb
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-27 20:11:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,14 +28,23 @@ def get_english_meaning(file_path, emoticon)
       # if it falls out of the loop and never 'returned'
     # it means it is not in the load_library
     return "Sorry, that emoticon was not found"
-    
-  # language = emoticon_hash.key(emoticon)
-  # emoticon_hash.key(language)
   
 end
 
 
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
+   emoticon_hash = load_library(file_path)
+  
+  emoticon_hash.each do |key, value|
+    
+    if emoticon_hash[key][:english] == emoticon
+      return emoticon_hash[key][:japanese]
+    end
+  
+  end
+      # if it falls out of the loop and never 'returned'
+    # it means it is not in the load_library
+    return "Sorry, that emoticon was not found"
   
 end
